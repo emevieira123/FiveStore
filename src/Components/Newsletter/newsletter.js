66b1c0ca1a430f4d04a38d3cae80762b1241af53
Emerson Vieira <emevieira@gmail.com>
2021-07-17 01:20:14 -0400

    Finalizado implementacao de contato

--- src/Components/Newsletter/newsletter.js
+++ src/Components/Newsletter/newsletter.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 export default function Newsletter() {
+  const [usuario, setUsuario] = useState('');
+  const [email, setEmail] = useState('');
+
+  function handleInscrever(e) {
+
+    e.preventDefault();
+
+    if (usuario && email != null) {
+      localStorage.setItem('Name', JSON.stringify(usuario));
+      localStorage.setItem('E-mail', JSON.stringify(email));
+      alert('Cadastrado com sucesso!')
+    } else {
+      alert('ERROR - Verifique os dados e tente novamente!')
+    }
+
+  }
+
   return (
     <div className="newsletter">
       <h1>CADASTRE-SE PARA RECEBER AS OFERTAS DO BLACK FRIDAY!</h1>
@@ -11,9 +28,9 @@ export default function Newsletter() {
 
       <div className="formNewsletter">
         <form>
-          <input type="text" placeholder="Nome" required />
-          <input type="email" placeholder="E-mail" required />
-          <button>Inscrever</button>
+          <input type="text" placeholder="Nome" value={usuario} onChange={e => setUsuario(e.target.value)} required />
+          <input type="email" placeholder="E-mail" value={email} onChange={e => setEmail(e.target.value)} required />
+          <button type="button" onClick={handleInscrever}>Inscrever</button>
         </form>
       </div>
     </div>
--- src/Pages/Contact/contact.js
+++ src/Pages/Contact/contact.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { FaLinkedin, FaGithub, FaFacebook, FaWhatsapp } from 'react-icons/fa';
+import { GrInstagram } from 'react-icons/gr';
+import { IoLocationSharp } from 'react-icons/io5';
+import { HiOutlineMail } from 'react-icons/hi';
 
 import './style.css'
 
 export default function Contact() {
   return (
     <div className="contactBody">
-      <h1>Contato</h1>
+
+      <div className="gripContact">
+        <h2 className="tituloPatrocinio">Patrocínio</h2>
+        <img className="imgPatrocinio" src="./assets/logoVTEX.svg" alt="" />
+        <img className="imgPatrocinio" src="./assets/logoGama.png" alt="" />
+      </div>
+
+      <div className="gripContact">
+        <h2 className="tituloSociais">Redes Sociais</h2>
+        <div className="containerIcon">
+          <a href="https://www.linkedin.com/in/emerson-vieira-9416267b/" target="_blank" rel="noreferrer">
+            <FaLinkedin className="iconSociais" />
+          </a>
+          <a href="https://github.com/emevieira123/" target="_blank" rel="noreferrer">
+            <FaGithub className="iconSociais" />
+          </a>
+          <a href="https://www.facebook.com/emersonvie/" target="_blank" rel="noreferrer">
+            <FaFacebook className="iconSociais" />
+          </a>
+          <a href="https://www.instagram.com/emerson_vchaves/" target="_blank" rel="noreferrer">
+            <GrInstagram className="iconSociais" />
+          </a>
+        </div>
+      </div>
+
+      <div className="gripContact">
+        {/* <h2 className="tituloContact">Contato</h2> */}
+        <div className="contact">
+          <div className="contactInterna">
+            <HiOutlineMail className="iconContact" />
+            <p>emevieira@gmail.com</p>
+          </div>
+          <div className="contactInterna">
+            <FaWhatsapp className="iconContact" />
+            <p>(65) 99258-4915</p>
+          </div>
+          <div className="contactInterna">
+            <IoLocationSharp className="iconContact" />
+            <p>Mato-Grosso</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="footer">
+        © Copyright 2021 | Todos os direitos reservados.
+      </div>
     </div>
   );
 }
